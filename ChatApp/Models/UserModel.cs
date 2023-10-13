1be336cfc44c3b020737dcff3d5784a38f74b992
Anthony Huang <Huang.Anthony@outlook.com>
2023-10-12 20:47:36 -0400

    Added Docker Compose

---
+++ ChatApp/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
